e637d19ec562b8efececd32f50d4fc886929170f
ido sarue <dodojoker67@gmail.com>
2022-02-22 19:40:16 +0200

    test

--- ninjaTutorials/myWork/use_cli/vuejs-playlist/src/main.js
+++ ninjaTutorials/myWork/use_cli/vuejs-playlist/src/main.js
@@ -1,9 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
-import Ninjas from "./components/Ninjas.vue";
 
-// Global component nesting
-// Vue.component("ninjas", Ninjas);
+// Event bus
+export const bus = new Vue();
 
 new Vue({
 	el: "#app",
