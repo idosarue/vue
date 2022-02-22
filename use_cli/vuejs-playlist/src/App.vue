37859cecae69a72fc67165c7d26047adb1aed18c
ido sarue <dodojoker67@gmail.com>
2022-02-22 15:17:58 +0200

    started learning nesting

---
+++ use_cli/vuejs-playlist/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+  <p></p>
+    <h1>{{ title }}</h1>
+    <p>{{ greeting() }}</p>
+    <ninjas></ninjas>
+  </div>
+</template>
+
+<script>
+
+import Ninjas from './Ninjas.vue'
+
+export default {
+  // Local nesting
+    components : {
+      Ninjas
+    },
+    data() {
+        return {
+            title: "Ninjas",
+        };
+    },
+    methods: {
+        greeting: function () {
+            return "heyy cowboy!";
+        }
+    },
+};
+</script>
+
+
+<style></style>
---
+++ use_cli/vuejs-playlist/src/Ninjas.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <ul>
+      <li v-for="ninja in ninjas">{{ ninja }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "Ninjas",
+      ninjas: ["Yoshi", "Mario", "Luigi"]
+    };
+  }
+};
+</script>
+
+<style></style>
