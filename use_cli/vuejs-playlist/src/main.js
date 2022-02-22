082a7693b4ddf710ce213517cff862e320447195
ido sarue <dodojoker67@gmail.com>
2022-02-22 18:22:50 +0200

    completed lesson 24

--- use_cli/vuejs-playlist/src/main.js
+++ use_cli/vuejs-playlist/src/main.js
@@ -1,11 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
-import Ninjas from "./Ninjas.vue";
+import Ninjas from "./components/Ninjas.vue";
 
 // Global component nesting
 // Vue.component("ninjas", Ninjas);
 
 new Vue({
-  el: "#app",
-  render: h => h(App)
+	el: "#app",
+	render: (h) => h(App),
 });
