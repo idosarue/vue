829cca2b04df9a5864d733e4b6bcc832998894d8
ido sarue <dodojoker67@gmail.com>
2022-02-24 14:31:17 +0200

    completed lesson 29

--- ninjaTutorials/myWork/use_cli/vuejs-playlist/src/App.vue
+++ ninjaTutorials/myWork/use_cli/vuejs-playlist/src/App.vue
@@ -1,37 +1,26 @@
 <template>
 	<div>
-		<keep-alive>
-			<component v-bind:is="component"></component>
-		</keep-alive>
-		<button v-on:click="component = 'form-one'">Show Form One</button>
-		<button v-on:click="component = 'form-two'">Show Form Two</button>
-		<!-- With function -->
-		<button v-on:click="switchForms" style="background-color: red;">Switch forms</button>
-		<!-- **** -->
+		<add-blog></add-blog>
 	</div>
 </template>
 
 <script>
-	import FormOne from "./components/FormOne.vue";
-	import FormTwo from "./components/FormTwo.vue";
+	import AddBlog from "./components/AddBlog.vue";
 
 	export default {
-		// Local nesting
 		components: {
-			"form-one": FormOne,
-			"form-two": FormTwo,
+			"add-blog": AddBlog,
 		},
 		data() {
-			return {
-				component: "form-one",
-			};
+			return {};
 		},
-		methods: {
-			switchForms() {
-				this.component = this.component === "form-one" ? "form-two" : "form-one";
-			},
-		},
+		methods: {},
 	};
 </script>
 
-<style></style>
+<style>
+	body {
+		margin: 0;
+		font-family: "Nunito SemiBold";
+	}
+</style>
