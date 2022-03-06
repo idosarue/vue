735f4c3b14093740913225e894dc808674bb0eb4
ido sarue <dodojoker67@gmail.com>
2022-03-06 16:02:40 +0200

    added pup up notification, edit deltete and add options

--- contactsProj/contacts/src/src/routes.js
+++
@@ -1,7 +0,0 @@
-import SingleContact from "./components/SingleContact.vue";
-import ShowContacts from "./components/ShowContacts.vue";
-
-export default [
-	{ path: "/contact/:id", component: SingleContact },
-	{ path: "/contacts", component: ShowContacts },
-];
